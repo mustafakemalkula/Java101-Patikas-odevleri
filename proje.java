017f7ade23b4a2da776b02dc3b9ee7540e4d3015
mustafakemalkula <110534128+mustafakemalkula@users.noreply.github.com>
2022-09-19 12:13:30 +0300

    Add files via upload

--- proje.java
+++ proje.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ${NAME} {
+public class proje {
 
     public static void main(String[] args) {
 Scanner input = new Scanner(System.in);
@@ -10,10 +10,13 @@ double a,b, c, kdv;
         System.out.println("KDV Hesaplayıcısına Hoşgeldiniz!");
         System.out.println("Ürünün Fiyatı Nedir? ");
         a = input.nextDouble();
-        kdv = a>=1000&&a>=0 ? 18 : 8;
-        b = (a*kdv)/100;
-        System.out.println("Girdiğiniz ürünün KDV tutarı: " + b);
-        c = a+b;
+        kdv = a>=1000 ? 0.8 : 0.18;
+        System.out.println("Girdiğiniz ürünün KDV oranı: " + kdv);
+        b = a*kdv;
+            System.out.println("Girdiğiniz ürünün KDV tutarı: " + b);
+
+        c = b +a;
+
         System.out.println("Ürünün KDV'li fiyatı: " + c);
         System.out.println("Bizi tercih ettiğiniz için teşekkürler!");
 
